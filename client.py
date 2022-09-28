f95dc15013afee1c9bc2b3fa9cdf9283e326f655
Christian Ocampo Campuzano <christianocampo@usf.edu>
2022-09-27 23:48:24 -0400

    The value of the price variable in the getDataPoint function was changed so that it gave the average for the bid and asking price of a stock.
    The getRatio function was fixed so that it gave a ratio between the
    prices of the two stocks. Additionally, an if statement was created to
    prevent the possibility of a division by zero when performing the ratio.
    
    Inside Main, a dictionary was created to store the prices of the two
    stocks. Inside the nested for loop, the dictionary is added with these
    values. Lastly, the final print statement has been changed so that it
    grabs and prints the prices for ABC and DEF.

--- client.py
+++ client.py
@@ -35,14 +35,20 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    # As stated in the task information, the price of a stock is given
+    # by finding the average between its bid and asking price.
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    # To avoid raising an exception for dividing by zero, the function
+    # is returned if price_b is zero.
+    if price_b == 0:
+        return
+    return price_a / price_b
 
 
 # Main
@@ -52,8 +58,13 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        # An empty dictionary is created to store the prices of the two
+        # stocks and send them to the getRatio function to later print
+        # the ratio between the two.
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
